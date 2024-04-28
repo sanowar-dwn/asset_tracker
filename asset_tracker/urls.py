9d3a9e5bc3997e0618db781a73c4e97574b92544
sanowar-dwn <sanowardwnofficial@gmail.com>
2024-04-28 12:30:10 +0600

    swagger-ui added

--- asset_tracker/urls.py
+++ asset_tracker/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.schemas import get_schema_view
+from django.views.generic.base import TemplateView
 urlpatterns = [
+    path('api_schema', get_schema_view(title='API Schema'), name='api_schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name="docs.html",
+        extra_context={'schema_url':'api_schema'}
+        ), name= 'swagger-ui'),
     path('admin/', admin.site.urls),
-    path('', include('api.company.urls')),
+    path('', include('api.company.urls')), #Company App APIs here
 ]
